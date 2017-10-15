24f717decb0587178d2b4bbb4a5226b47fe4946f
wilxsv <wixl.sv@gmail.com>
2017-10-15 15:31:05 -0600

    actualizada vista show de persona y vista personal de usuaria

--- src/Minsal/SuministroBundle/Controller/DefaultController.php
+++ src/Minsal/SuministroBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Minsal\SuministroBundle\Entity\MntInsumo;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class DefaultController extends Controller
@@ -15,29 +16,41 @@ class DefaultController extends Controller
 	
     public function indexAction()
     {
+		$agrega = null;
+		$valida = null;
+		$habilita = null;
+		$denegados = null;
 		$auth_checker = $this->get('security.authorization_checker');
 		
 		if ($auth_checker->isGranted('IS_AUTHENTICATED_FULLY')) {
 			$this->setAccess($auth_checker);
+			$repository = $this->getDoctrine()->getRepository('MinsalSuministroBundle:MntInsumo');
+			$query = $repository->createQueryBuilder('i')->where('i.estadoSchema = -1 OR i.enableSchema = -1')->addOrderBy('i.registroSchema', 'ASC')->getQuery();
+			$denegados = $query->getResult();
+			if ($this->agrega){
+				
+			}
+			if ($this->valida){
+			}
+			if ($auth_checker->isGranted('ROLE_UNABAST')){
+			}
 		}
-
-        return $this->render('MinsalSuministroBundle:Default:index.html.twig', array('mntInsumos' => null,'agrega' => $this->agrega,));
+		return $this->render('MinsalSuministroBundle:Default:index.html.twig', array('agrega' => $agrega, 'valida' => $valida, 'habilita' => $habilita, 'denegados' => $denegados));
     }
     
     public function setAccess($rol){
 		//$grupos
 		$em = $this->getDoctrine()->getManager();
 		$data = '0';
+		$menu = '';
 		
-		$roles =  $em->createQueryBuilder()
-			->select('g.id, g.nombreGrupo, r.nombreRol')
-			->from('MinsalSuministroBundle:CtlGrupo', 'g')
-			->innerJoin('g.roles','r')
-			->getQuery()
-			->getArrayResult();
+		$roles =  $em->createQueryBuilder()->select('g.id, g.nombreGrupo, r.nombreRol')->from('MinsalSuministroBundle:CtlGrupo', 'g')->innerJoin('g.roles','r')->getQuery()->getArrayResult();
+		$menus =  $em->createQueryBuilder()->select('a.nombreAcceso, a.pathAcceso, r.nombreRol')->from('MinsalSuministroBundle:SegAcceso', 'a')
+		->innerJoin('a.role','r')->getQuery()->getArrayResult();
 		foreach ($roles as $rolt) {	
 			if ( $rol->isGranted( $rolt["nombreRol"] ) ){
 				$data .= ','.$rolt["id"];
+				$this->agrega = true;
 				/*
 				$data['accion'] = 'valida';
 				$this->get('session')->set('accionvalida', TRUE );
@@ -48,8 +61,15 @@ class DefaultController extends Controller
 				$lista .= $rolt["id"].",";*/
 			}
 		}
+		foreach ($menus as $m) {	
+			if ( $rol->isGranted( $m["nombreRol"] ) ){
+				$url = $this->generateUrl($m['pathAcceso'], array());
+				$menu .= '<li><a href="'.$url.' class="waves-effect"><i class="mdi " ></i><span class="hide-menu">'.$m["nombreAcceso"].'</span></a></li>';
+			}
+		}
 		$this->get('session')->set('accion', $roles);
 		$this->get('session')->set('agrega', $data);
+		$this->get('session')->set('menu', $menu);
 		
 	}
     
